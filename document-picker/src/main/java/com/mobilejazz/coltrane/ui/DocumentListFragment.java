762275dc980118f1b11e7824144b138c48ab9472
XuluWarrior <github@metaliving.plus.com>
2021-03-15 13:11:47 +0000

    migrate to androidx

--- document-picker/src/main/java/com/mobilejazz/coltrane/ui/DocumentListFragment.java
+++ document-picker/src/main/java/com/mobilejazz/coltrane/ui/DocumentListFragment.java
@@ -23,8 +23,8 @@ import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.ContentLoadingProgressBar;
-import android.support.v4.widget.SwipeRefreshLayout;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
